9189774ce5e842fdd4748c42e37fa25894485e1f
chandramogali <chandragomasi97@gmail.com>
2022-07-06 22:31:01 +0530

    deploy sort-visualizer

---
+++ src/components/molecules/Footer/index.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import './style.css';
+
+const Footer = (props) => {
+  return (
+    <footer className="Footer">
+      <section>
+        Designed and built with{' '}
+        <span className="Footer__Heart">&hearts;</span> by{' '}
+        <a
+          href="https://www.linkedin.com/in/chandramogali-gomasi-04888120a/"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="Footer__Link"
+        >
+          Chandramogali Gomasi
+        </a>
+      </section>
+
+      <section className="Footer__Items">
+        {/* <a
+          href="https://youtu.be/JFjvVmvC3pQ"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="Footer__Link"
+        >
+          Demo
+        </a> */}
+        <a
+          href="https://github.com/chandramogali"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="Footer__Link"
+        >
+          Code
+        </a>
+      </section>
+    </footer>
+  );
+};
+
+export default Footer;
